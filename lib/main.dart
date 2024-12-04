265bc3874b4f347f45affb539be7cf28e5d00944
DESKTOP-VD4OAC8\m <nadahamdy09912@gmail.com>
2024-12-04 20:55:55 +0200

    -connected firebase with the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:chat_app/core/di/dependency_injection.dart';
 import 'package:chat_app/features/splash/presentation/views/splashScreen.dart';
+import 'package:chat_app/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+ await setUpGet();
   await ScreenUtil.ensureScreenSize();
+ 
   runApp(const MyApp());
 }
 
@@ -13,6 +23,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FirebaseAuth.instance
+  .authStateChanges()
+  .listen((User? user) {
+    if (user == null) {
+      print('=================User is currently signed out!=================');
+    } else {
+      print('=================User is signed in!=================');
+    }
+  });
     return const ScreenUtilInit(
       designSize: Size( 375,812),
       minTextAdapt: true,
